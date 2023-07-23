90f7c1fb9d23e9560d9b5eb451a2e865c8531fd7
ShikiSuen <shikisuen@outlook.com>
2023-07-23 19:42:24 +0800

    Add clang-format scripts and more gitignore items.

---
+++ format.sh
@@ -0,0 +1,8 @@
+find . -regex '.*\.\(hh\)' -exec clang-format -style=file -i {} \;
+find . -regex '.*\.\(cc\)' -exec clang-format -style=file -i {} \;
+find . -regex '.*\.\(mm\)' -exec clang-format -style=file -i {} \;
+find . -regex '.*\.\(c\)' -exec clang-format -style=file -i {} \;
+find . -regex '.*\.\(h\)' -exec clang-format -style=file -i {} \;
+find . -regex '.*\.\(m\)' -exec clang-format -style=file -i {} \;
+find . -regex '.*\.\(cpp\)' -exec clang-format -style=file -i {} \;
+find . -regex '.*\.\(hpp\)' -exec clang-format -style=file -i {} \;
